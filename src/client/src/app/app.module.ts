f59128f1cea3c5a8951761ada1d7110a00005b50
Bryce <71417376+brycejjacks@users.noreply.github.com>
2021-10-26 10:46:40 -0700

    feat(client): add home and sign-in page

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
