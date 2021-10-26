201810e8fdfead70d3c0d8d46b0b7059db889a86
Bryce <71417376+brycejjacks@users.noreply.github.com>
2021-10-26 10:32:03 -0700

    feat(server): install mongoose

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import path from 'path';
+import mongoose from 'mongoose';
 
 const __dirname = path.resolve();
 console.log(__dirname);
@@ -8,13 +9,19 @@ const port = 3000;
 
 const app = express();
 
+mongoose
+  .connect("mongodb://localhost:27017/test")
+  .then(() => {
+    console.log("Connected to DB Successfully");
+  })
+  .catch((err) => console.log("Failed to Connect to DB", err));
+
 app.use(cors());
+app.use(express.static("public"));
+
 
 app.get('/', function(req, res) {
   res.json({test: 'test'})
-});
-app.get('/users', function(req, res) {
-  res.sendFile(path.join(__dirname, 'users.json'));
 });
 
 app.listen(port, function() {
