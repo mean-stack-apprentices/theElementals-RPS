db0529b09d630ac1cb0009f180f0f7869bd2cec4
Bryce <71417376+brycejjacks@users.noreply.github.com>
2021-10-26 12:40:05 -0700

    feat(client): init for future pages and components

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { PlayComponent } from './components/play/play.component';
+import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SignInComponent
+    SignInComponent,
+    PlayComponent,
+    LeaderboardComponent
   ],
   imports: [
     BrowserModule,
