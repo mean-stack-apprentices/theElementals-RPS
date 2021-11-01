df3e0a59d84c7786b6e96b6709c0a23f62d204b1
Bryce <71417376+brycejjacks@users.noreply.github.com>
2021-11-01 16:05:43 -0700

    feat(client): add login input

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
 import { PlayComponent } from './components/play/play.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
   {path:'home', component: HomeComponent,
     children: [
       {path:'play', component: PlayComponent},
       {path:'leaderboard', component: LeaderboardComponent},
+      {path:'sign-in', component: SignInComponent},
     ]
   },
+  {path: 'sign-up', component: SignUpComponent}
 ];
 
 @NgModule({
--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { PlayComponent } from './components/play/play.component';
 import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SignInComponent,
     PlayComponent,
-    LeaderboardComponent
+    LeaderboardComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
