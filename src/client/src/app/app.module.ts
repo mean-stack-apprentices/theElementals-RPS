18b9ca350183a848b97addcf3e0366487651b503
Bryce <71417376+brycejjacks@users.noreply.github.com>
2021-11-01 19:38:10 -0700

    feat(client): add services.. api, user

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { SignUpComponent } from './pages/sign-up/sign-up.component';
     PlayComponent,
     LeaderboardComponent,
     SignInComponent,
-    SignUpComponent
+    SignUpComponent,
+    HttpClientModule,
   ],
   imports: [
     BrowserModule,
