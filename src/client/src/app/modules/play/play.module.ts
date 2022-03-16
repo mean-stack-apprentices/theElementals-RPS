0efc211ee21160c532b38e2749d52a7fbc541ec1
Gichelle Amon <amongichelle@gmail.com>
2022-03-16 11:13:13 -0700

    feat(play, client): generate play module and routing

---
+++ src/client/src/app/modules/play/play.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PlayRoutingModule } from './play-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PlayRoutingModule
+  ]
+})
+export class PlayModule { }
