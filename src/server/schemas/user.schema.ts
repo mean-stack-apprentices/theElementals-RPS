ff4c9a95fa2416bf83f79b0a1941142a71f20df6
brycejjacks <brycejjacks@gmail.com>
2022-03-29 13:55:39 -0700

    ref(user.schema): remove profilePic

--- src/server/schemas/user.schema.ts
+++ src/server/schemas/user.schema.ts
@@ -4,7 +4,6 @@ import type { User } from "../../shared/models/user.model";
 const userSchema = new mongoose.Schema<User>({
     username:{type: String, required: true},
     password:{type: String, required: true},
-    //profilePic:{type: Image},
 })
 
 export const UserModel = mongoose.model<User>('user', userSchema);
