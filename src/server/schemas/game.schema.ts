79685b3398981285a5a9111c245db990c4a4db0c
Gichelle Amon <amongichelle@gmail.com>
2022-03-30 13:59:29 -0700

    ref(schema, model): remove 'pin' and 'type' properties

--- src/server/schemas/game.schema.ts
+++ src/server/schemas/game.schema.ts
@@ -4,11 +4,9 @@ import type { Game } from '../../shared/models/game.model';
 const {Schema, model} = mongoose;
 
 const gameModel = new Schema<Game>({
-    pin: { type: String },
     date: { type: Date },
     players: [{type: mongoose.Types.ObjectId, ref: "user"}],
     winner: {type: mongoose.Types.ObjectId, ref: "user"},
-    type: {type: String, enum: ["Game", "Tournament"]},
 
 })
 
--- src/shared/models/game.model.ts
+++ src/shared/models/game.model.ts
@@ -1,11 +1,9 @@
-import type mongoose from "mongoose";
+import mongoose from "mongoose";
 import { User } from '../models/user.model';
 
 export interface Game {
-    pin: string,
     date: Date,
     players: [type: mongoose.Types.ObjectId | User],
     winner: {type: mongoose.Types.ObjectId | User},
-    type: string,
     _id: string,
 }
