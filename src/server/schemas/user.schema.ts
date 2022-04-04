a37a43121e0d620d1f76a78e792a2cae28772cd2
brycejjacks <brycejjacks@gmail.com>
2022-04-04 14:23:08 -0700

    ref(user.schema): match user.model

--- src/server/schemas/user.schema.ts
+++ src/server/schemas/user.schema.ts
@@ -4,6 +4,7 @@ import type { User } from "../../shared/models/user.model";
 const userSchema = new mongoose.Schema<User>({
     username:{type: String, required: true},
     password:{type: String, required: true},
+    profilePic:{type: mongoose.Schema.Types.Mixed}
 })
 
 export const UserModel = mongoose.model<User>('user', userSchema);
