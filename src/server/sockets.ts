a0adaef3c590cb72799fc28dfa71affd7c81f5d6
Gichelle Amon <amongichelle@gmail.com>
2022-04-27 11:21:00 -0700

    feat(socket, server):emit pin to client after user created tournament

--- src/server/sockets.ts
+++ src/server/sockets.ts
@@ -1,11 +1,32 @@
 import { io } from './configs/socket.config.js';
+import { generateTournamentPin } from './helpers/socket.helper.js';
+
+const tournamentPool: any = {
+  // tPin: [
+  //     socketIds,
+  //     socketIds,
+  // ],
+    // tPin: [
+  //     socketIds,
+  //     socketIds,
+  // ]
+}
 
 export default io.on("connection", (socket) => {
     console.log("user connected, ", socket.id)
 
-    socket.on('create-room', (room) => {
-      socket.join(room)
-      console.log('id: ', room)
+    socket.on('create-tournament', () => {
+      let tPin = generateTournamentPin()
+      let joinedPlayers: any
+      let tournamentKey  = tPin.toString()
+      socket.emit('generate tournament pin', {tournamentPin: tPin })
+      socket.join(tournamentKey)
+
+          if ( tournamentPool[tournamentKey] == undefined){
+            joinedPlayers = new Array<string>(socket.id)
+            tournamentPool[tPin] = joinedPlayers
+          }
+          console.log(tournamentPool)
     })
   
     socket.on("disconnect", () => {
