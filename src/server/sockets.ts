7a86dfbbf1bc700a68692d1799c75387ca16c464
brycejjacks <brycejjacks@gmail.com>
2022-09-12 11:40:49 -0700

    ref(sever): add function and rework shape of data

--- src/server/sockets.ts
+++ src/server/sockets.ts
@@ -1,5 +1,7 @@
+import { Player } from '../shared/models/player.model.js';
 import { io } from './configs/socket.config.js';
 import * as socketHelpers from './helpers/socket.helper.js';
+import * as computerHelper from './helpers/computer.helper.js';
 
 
 // CONNECTION STATES
@@ -15,8 +17,9 @@ const tournamentPool: any = {
   // ]
 }
 
-const createFindPool: any = {
+const gamePool: any = {
   /*__pin__: {
+    isVsComputer: boolean
     pLeft: __Player__,
     pRight: __Player__,
   }
@@ -26,80 +29,102 @@ const createFindPool: any = {
 export default io.on("connection", (socket) => {
     console.log("user connected, ", socket.id)
 
-    socket.on('requesting to create match',(payload) => {
-      const {emittingPlayer} = payload
-      let pin = socketHelpers.getUniqueGamePin(createFindPool, socketHelpers.randomPin())
-      createFindPool[pin] = {pLeft: emittingPlayer}
-      socket.join(pin)
-      socket.emit('set game pin', pin)
-      socket.emit('route',  'online-match/game-pin-display')
-    })
     socket.on('find created game', (payload, clientCallback)=>{
       const {emittingPlayer, requestedGamePin} = payload
-      if (!createFindPool[requestedGamePin]) {
-        console.log('SOME THING IS VERY WRONG')
+      if (!gamePool[requestedGamePin]) {
+        console.log('SOME THING IS VERY WRONG, GAME DOES NOT EXIST')
       } else {
+        clientCallback() // setting active player
         socket.join(requestedGamePin)
         socket.emit('set game pin', requestedGamePin)
-        clientCallback(createFindPool[requestedGamePin]) // setting active player
-        createFindPool[requestedGamePin].pRight = emittingPlayer
-        io.to(requestedGamePin).emit('update game players', createFindPool[requestedGamePin])
+        gamePool[requestedGamePin].pRight = emittingPlayer
+        io.to(requestedGamePin).emit('update game players', gamePool[requestedGamePin])
         io.to(requestedGamePin).emit('route', 'game')
       
       }
     })
     socket.on('create-tournament', (socketID, cb) => {
       let tPin = socketHelpers.getUniqueGamePin(tournamentPool, socketHelpers.randomPin())
-      socket.join(tPin)         
+      socket.join(tPin)        
       let joinedPlayers = new Array(socket.id)
       tournamentPool[tPin] = joinedPlayers
       cb(tPin);
       
       console.log(tournamentPool)
     })
-    socket.on("request decrease player's health", payload => {
-      const {gamePin, side} = payload
-      if (!createFindPool[gamePin]){
-        console.log('SOME THING IS VERY WRONG')
-      } else {
-        createFindPool[gamePin][side].health -= 1
-        io.to(gamePin).emit('update game players', createFindPool[gamePin])
-      }
+    socket.on('requesting to create match',(payload) => {
+      const {emittingPlayer} = payload
+      let pin = socketHelpers.getUniqueGamePin(gamePool, socketHelpers.randomPin())
+      gamePool[pin] = {pLeft: emittingPlayer}
+      socket.join(pin)
+      socket.emit('set game pin', pin)
+      socket.emit('route', 'online-match/game-pin-display')
+    })
+    socket.on('requesting to play computer',(payload) => {
+      const {emittingPlayer} = payload
+      let pin = socketHelpers.getUniqueGamePin(gamePool, socketHelpers.randomPin())
+      gamePool[pin] = { isVsComputer: true, pLeft: emittingPlayer}
+      socket.join(pin)
+      socket.emit('set game pin', pin)
+      gamePool[pin].pRight = computerHelper.getComputerOpponent()
+      
+      io.to(pin).emit('update game players', gamePool[pin])
+      socket.emit('route', 'game')
     })
     socket.on('request set isStarted', (payload) => {
       const {gamePin, bool} = payload
-      if (!createFindPool[gamePin]){
-        console.log('SOME THING IS VERY WRONG')
+      if (!gamePool[gamePin]){
+        console.log('SOME THING IS VERY WRONG, GAME DOES NOT EXIST')
       } else {
-        createFindPool[gamePin].isStarted = bool
+        gamePool[gamePin].isStarted = bool
         io.to(gamePin).emit('set isStarted', bool)
       }
     })
     socket.on("request set player's selection", payload => {
       const {gamePin, side, selction} = payload
-      if (!createFindPool[gamePin]){
-        console.log('SOME THING IS VERY WRONG')
+      if (!gamePool[gamePin]){
+        console.log('SOME THING IS VERY WRONG, GAME DOES NOT EXIST')
       } else {
-        createFindPool[gamePin][side].optionSelction = selction
-        io.to(gamePin).emit('update game players', createFindPool[gamePin])
+        gamePool[gamePin][side].optionSelction = selction
+        gamePool[gamePin][side].ready = true
+        if (gamePool[gamePin].isVsComputer) {
+          //getComputerSelection()
+          //emitReady()
+          //manage resultObj()
+        } else if (socketHelpers.yallBothReady(gamePool[gamePin])) {
+          const losingSide: string = socketHelpers.findMeALoser(gamePool[gamePin])
+          switch(losingSide){
+            case 'pLeft':
+            case 'pRight':
+              gamePool[gamePin][losingSide].health -= 1
+              break
+            case 'draw':
+              break
+            default:
+              console.log(losingSide, "^^^Error Finding a Loser")
+          }
+          io.to(gamePin).emit('set result', socketHelpers.getResultObj(losingSide))
+        }
+        
+        io.to(gamePin).emit('update game players', gamePool[gamePin])
       }
     })
     socket.on('request set side to NOT ready', payload => {
       const {gamePin, side} = payload
-      if (!createFindPool[gamePin]){
-        console.log('SOME THING IS VERY WRONG')
+      if (!gamePool[gamePin]){
+        console.log('SOME THING IS VERY WRONG, GAME DOES NOT EXIST')
       } else {
-        createFindPool[gamePin][side].ready = false
-        io.to(gamePin).emit('update game players', createFindPool[gamePin])
+        gamePool[gamePin][side].ready = false
+        io.to(gamePin).emit('update game players', gamePool[gamePin])
       }
     })
     socket.on('request set side to ready', (payload) => {
       const {gamePin, side} = payload
-      if (!createFindPool[gamePin]){
-        console.log('SOME THING IS VERY WRONG')
+      if (!gamePool[gamePin]){
+        console.log('SOME THING IS VERY WRONG, GAME DOES NOT EXIST')
       } else {
-        createFindPool[gamePin][side].ready = true
-        io.to(gamePin).emit('update game players', createFindPool[gamePin])
+        gamePool[gamePin][side].ready = true
+        io.to(gamePin).emit('update game players', gamePool[gamePin])
       }
     })
 
