89fabd97b65a31f14ebf9cecaab7a905fb6e51ea
brycejjacks <brycejjacks@gmail.com>
2022-09-12 11:36:54 -0700

    ref(gameLogic): move game logic off client

--- src/client/src/app/modules/game/services/game.service.ts
+++ src/client/src/app/modules/game/services/game.service.ts
@@ -13,48 +13,4 @@ export class GameService {
     console.log(this.selctions[random])
     return this.selctions[random]
   }
-
-  findRoundWinner(pLeft: Player, pRight: Player) {
-    if (pLeft.optionSelction && pRight.optionSelction) {
-      const l = pLeft.optionSelction
-      const r = pRight.optionSelction
-
-      if (l === 'rock' && r === 'rock') {
-        return 'draw'
-      }
-      else if (l === 'rock' && r === 'paper') {
-        return "pRight"
-      }
-      else if (l === 'rock' && r === 'scissors') {
-        return "pLeft"
-      }
-
-      else if (l === 'paper' && r === 'rock') {
-        return "pLeft"
-      }
-      else if (l === 'paper' && r === 'paper') {
-        return "draw"
-      }
-      else if (l === 'paper' && r === 'scissors') {
-        return "pRight"
-      }
-
-      else if (l === 'scissors' && r === 'rock') {
-        return "pRight"
-      }
-      else if (l === 'scissors' && r === 'paper') {
-        return "pLeft"
-      }
-      else if (l === 'scissors' && r === 'scissors') {
-        return 'draw'
-      }else {
-        console.log('something went very wrong lol')
-        return 'something went very wrong lol'
-
-      }
-    } else {
-      console.log('something went very wrong lol')
-      return 'something went very wrong lol'
-    }
-  }
 }
--- src/server/helpers/socket.helper.ts
+++ src/server/helpers/socket.helper.ts
@@ -1,4 +1,74 @@
 
+
+function findMeALoser(game: {pLeft:any, pRight: any}) {
+    const l = game.pLeft.optionSelction
+    const r = game.pRight.optionSelction
+    if (l && r) {
+        if (l === r) {
+        return 'draw'
+        }
+
+        else if (l === 'rock' && r === 'paper') {
+        return "pLeft"
+        }
+        else if (l === 'rock' && r === 'scissors') {
+        return "pRight"
+        }
+
+        else if (l === 'paper' && r === 'rock') {
+        return "pRight"
+        }
+        else if (l === 'paper' && r === 'scissors') {
+        return "pLeft"
+        }
+
+        else if (l === 'scissors' && r === 'rock') {
+        return "pLeft"
+        }
+        else if (l === 'scissors' && r === 'paper') {
+        return "pRight"
+        }
+        
+        else {
+        console.log('something went very wrong, one or both users selected an option that is not rock paper or scissors')
+        return 'something went very wrong lol'
+
+        }
+    } else {
+        console.log('something went very wrong, both players dont have an option selected')
+        return 'something went very wrong lol'
+    }
+}
+function getResultObj(losingSide: string) {
+          let result = {}
+          switch(losingSide){
+            case 'pLeft':
+            case 'pRight':
+              result = {
+                draw: false,
+                winner: losingSide == "pLeft" ? "pRight" : "pLeft",
+                loser: losingSide == "pLeft" ? "pLeft" : "pRight"
+              }
+              break
+            case 'draw':
+              result = {
+                draw: true,
+                winner: null,
+                loser: null,
+              }
+              break
+            default:
+              console.log(losingSide, "^^^Error Finding a Loser")
+          }
+          return result
+}
+function getUniqueGamePin(pool: any, pin:string): string {
+    if (pool[pin]) {
+        return getUniqueGamePin(pool, randomPin())
+    } else {
+        return pin
+    }
+}
 function randomPin() {
     const length = 4;
     let result           = '';
@@ -9,12 +79,8 @@ function randomPin() {
     }
     return result;
 }
-  
-function getUniqueGamePin(pool: any, pin:string): string {
-    if (pool[pin]) {
-        return getUniqueGamePin(pool, randomPin())
-    } else {
-        return pin
-    }
+function yallBothReady(game: {pLeft: any, pRight: any}) {
+    return game.pLeft.ready && game.pRight.ready ? true : false
 }
-export { randomPin, getUniqueGamePin }
+
+export { findMeALoser, getResultObj, getUniqueGamePin, randomPin, yallBothReady }
