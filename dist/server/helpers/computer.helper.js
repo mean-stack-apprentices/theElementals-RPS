8db40a677862823b4bae79ce775596a1e97aa2f1
brycejjacks <brycejjacks@gmail.com>
2022-09-13 09:43:51 -0700

    feat(computerLogic): add selection function

--- dist/server/helpers/computer.helper.js
+++ dist/server/helpers/computer.helper.js
@@ -1,4 +1,5 @@
 import { Player } from "../../shared/models/player.model.js";
+const selctions = ['rock', 'paper', 'scissors'];
 const POSSIBLE_COMPUTER_NAMES = [
     'Carson',
     'Cam',
@@ -12,9 +13,14 @@ const POSSIBLE_COMPUTER_NAMES = [
 function getComputerOpponent() {
     return new Player(getComputerUsername());
 }
+function getRandomSelection() {
+    const random = Math.floor(Math.random() * 3);
+    console.log(selctions[random]);
+    return selctions[random];
+}
 function getComputerUsername() {
     const randomNameIndex = Math.floor(Math.random() * POSSIBLE_COMPUTER_NAMES.length);
     return 'Computer' + POSSIBLE_COMPUTER_NAMES[randomNameIndex];
 }
-export { getComputerOpponent };
+export { getComputerOpponent, getRandomSelection };
 //# sourceMappingURL=computer.helper.js.map
--- src/server/helpers/computer.helper.ts
+++ src/server/helpers/computer.helper.ts
@@ -1,5 +1,6 @@
 import { Player } from "../../shared/models/player.model.js"
 
+const selctions: ('rock' | 'paper' | 'scissors')[] = ['rock', 'paper', 'scissors']
 const POSSIBLE_COMPUTER_NAMES: string[] = [
     'Carson',
     'Cam',
@@ -14,9 +15,14 @@ const POSSIBLE_COMPUTER_NAMES: string[] = [
 function getComputerOpponent() {
     return new Player(getComputerUsername())
 }
+function getRandomSelection() {
+    const random = Math.floor(Math.random()* 3)
+    console.log(selctions[random])
+    return selctions[random]
+}
 function getComputerUsername() {
     const randomNameIndex = Math.floor(Math.random() * POSSIBLE_COMPUTER_NAMES.length)
     return 'Computer' + POSSIBLE_COMPUTER_NAMES[randomNameIndex]
 }
 
-export { getComputerOpponent }
+export { getComputerOpponent, getRandomSelection }
